f4e37445186c6d54277d5031ed3c927770728284
riannbarbosa <riannbarbosa5@gmail.com>
2025-04-27 18:30:34 -0300

    fix: fix issues

--- another_afd/lexical_analyzer.py
+++ another_afd/lexical_analyzer.py
@@ -1,5 +1,5 @@
 from afd import *
-
+from tabulate import tabulate
 class LexicalAnalyzer:
     def __init__(self, afd):
         self.afd = afd
@@ -30,7 +30,7 @@ class LexicalAnalyzer:
     def classify_token(self, token):
         if token in ['count']:
             return 'VARIABLE'
-        elif token in ['while', '=']:
+        elif token in ['while', '=', ':']:
             return 'KEYWORD'
         elif token in ['<', '+']:
             return 'OPERATOR'
@@ -45,53 +45,83 @@ class LexicalAnalyzer:
         path = []
         current_state = self.afd['initial_state']
         
+        
         for line_number, line in enumerate(lines, 1):
             if not line.strip():  # Pula linhas vazias
                 continue
-                
             tokens = self.tokenize(line)
             for token in tokens:
-                if current_state in self.afd['transitions'] and token in self.afd['transitions'][current_state]:
-                    next_state = self.afd['transitions'][current_state][token]
-                    path.append(next_state)
-                    label = self.classify_token(token)
-                    self.symbol_table.append({
+                for char in token:
+                    if (current_state in self.afd['transitions'] 
+                        and char in self.afd['transitions'][current_state]):
+                        
+                        next_state = self.afd['transitions'][current_state][char]
+                        path.append(next_state)
+                        current_state = next_state
+                    else:
+                    # Transição inválida
+                        self.errors.append({
+                            'line': line_number,
+                            'token': token,
+                            'message': f'Não há transição para o token "{token}" no estado {current_state}'
+                        })
+                        break
+                label = self.classify_token(token)
+                self.symbol_table.append({
                     'line': line_number,
                     'identifier': token,
                     'label': label
-                     })
-                    current_state = next_state
-                else:
-                # Transição inválida
-                    self.errors.append({
-                        'line': line_number,
-                        'token': token,
-                        'message': f'No transition for token "{token}" from state {current_state}'
-                    })
-                    label = 'INVALID'
-                    
-                    self.symbol_table.append({
-                        'line': line_number,
-                        'identifier': token,
-                        'label': label
-                    })
-
-            # Verificar se terminou em um estado final
+                })      
         if current_state not in self.afd['final_states']:
             self.errors.append({
                 'line': line_number,
                 'token': '',
-                'message': f'Process ended in non-final state {current_state}'
+                'message': f'Processo terminal sem um estado final {current_state}'
             })
+                
+        return True, path, self.errors    
+    
+    def print_afd(self):
+        # Print the alphabet
+        print(' ')
         
-        return current_state in self.afd['final_states'], path, self.errors
-
-    # def define_
-
+        states = sorted(self.afd['transitions'].keys(), key=lambda x: int(x[1:]))
+        
+        alphabet = sorted(self.afd['alphabet'])
+        
+        table_data = []
+        for state in states:
+            row = [state]  
+            for symbol in alphabet:
+                next_state = ""
+                for char in symbol: 
+                    if state in self.afd['transitions'] and char in self.afd['transitions'][state]:
+                        next_state = self.afd['transitions'][state][char]
+                        break  
+                row.append(next_state)
+            table_data.append(row)
+        
+        headers = ["Estado"] + alphabet
+        
+        print("\nAFD:")
+        print(tabulate(table_data, headers=headers, tablefmt="grid"))
+        
+        print("\nEstado Inicial:", self.afd['initial_state'])
+        
+        print("\nEstado final:", ', '.join(self.afd['final_states']))
+        
+        
+          
     def print_symbol_table(self):
-        print(self.errors)
-        print("\nSymbol Table:")
-        print("Line\tIdentifier\tLabel")
+        if not self.errors:
+            print("Nenhum erro encontrado.")
+            return
+        
+        print("Erro encontrado:")
+        for error in self.errors:
+            print(f"Linha {error['line']}: Erro com o token'{error['token']}' - {error['message']}")
+        print("\nTabela:")
+        print("Linha\tIdentificador\tRótulo")
         print("-" * 40)
         for symbol in self.symbol_table:
             print("{}\t{}\t\t{}".format(
--- another_afd/main.py
+++ another_afd/main.py
@@ -7,3 +7,4 @@ from lexical_analyzer import LexicalAnalyzer
 lexical_analyzer = LexicalAnalyzer(afd)
 symbol_table = lexical_analyzer.transitions("input.in")
 lexical_analyzer.print_symbol_table()   
+# lexical_analyzer.print_afd()
